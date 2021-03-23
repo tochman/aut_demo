9f863c0558f9cd74bdea4b2c96c1146281765422
Thomas <thomas@craftacademy.se>
2021-03-23 14:26:25 +0100

    book model tested Yay!

--- specs/models/Book.spec.js
+++ specs/models/Book.spec.js
@@ -1,19 +1,49 @@
 const { expect, factory } = require('../helpers')
+const { DataTypes } = require('sequelize')
 
-describe.only('Book', () => {
-  let subject
+describe('Book', () => {
+  BookModel = factory.factories.Book.Model
+  const { tableName, tableAttributes } = BookModel
 
   beforeEach(async () => {
-    debugger
     subject = await factory.create('Book')
   });
 
+  describe.only('Model', () => {
+    it('is expected to have a valid factory', () => {
+      expect(subject).to.include({
+        title: 'Whatever'
+      })
+    });
 
-  it('is expected to have a valid factory', () => {
+    it('is expected to have table name "Books', () => {
+      expect(tableName).to.equal('Books')
+    });
 
-    expect(subject).to.include({
-      title: 'Whatever',
-    })
+    describe('is expected to have property:', () => {
+
+      it('title:string', () => {
+        expect(tableAttributes)
+          .to.have.own.property('title')
+          .that.has.property('type').to.be.instanceOf(DataTypes.STRING)
+      });
+
+      it('createdAt:string', () => {
+        expect(tableAttributes)
+          .to.have.own.property('createdAt')
+          .that.has.property('type').to.be.instanceOf(DataTypes.DATE)
+      });
+
+      it('updatedAt:string', () => {
+        expect(tableAttributes)
+          .to.have.own.property('updatedAt')
+          .that.has.property('type').to.be.instanceOf(DataTypes.DATE)
+      });
+
+    });
   });
 
+  describe('Instance', () => {
+
+  });
 });
